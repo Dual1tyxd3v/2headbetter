75fcfa743e0694a5d6762cbac7ffd1fedbcf83bb
Dual1tyxd3v <d3vilc0re@gmail.com>
2023-11-17 23:50:39 +0700

    Added base settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Main from './pages/main';
+import Login from './pages/login';
+import Settings from './pages/settings';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
 function App() {
   return (
-    <div>Some test</div>
-  )
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/global-styles.ts
@@ -0,0 +1,66 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+:root {
+  --color-main-bg: #222222;
+}
+
+*,
+*::before,
+&::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  font-size: 1.6rem;
+  font-family: sans-serif;
+  line-height: 1.5;
+  background-color: var(--color-main-bg);
+  color: #fff;
+}
+
+img, canvas {
+  max-width: 100%;
+}
+
+input,
+button,
+textarea,
+select {
+  font: inherit;
+  color: inherit;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+ul {
+  list-style: none;
+}
+
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  overflow-wrap: break-word;
+  hyphens: auto;
+}
+
+button {
+  border: none;
+  outline: none;
+}
+`;
+
+export default GlobalStyles;
